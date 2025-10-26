a304ed014a1c835f0509f2afb74df4d370f72974
vedantika varnekar <varnekarvedantika.com>
2025-10-26 11:48:20 +0530

    Model Trainer

--- scr/components/data_ingestion.py
+++ scr/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from scr.components.data_transformation import DataTransformation
 from scr.components.data_transformation import DataTransformationConfig
 
+from scr.components.model_trainer import ModelTrainerConfig
+from scr.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -54,4 +57,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_Ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
+
+    Modeltrainer=ModelTrainer()
+    print(Modeltrainer.initiate_model_trainer(train_arr,test_arr))
